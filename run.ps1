33dad3e14d82b2e69d4e51fa7801f95af6e6b29a
Kalebpc <113309697+kalebpc@users.noreply.github.com>
2025-12-18 10:11:12 -0500

    Update video source and preset settings in run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,10 @@
 # handbrake.ps1 © 2025 https://github.com/kalebpc/handbrakecli
 [String]$preset1 = "Roku 480p30 Modded"
-[String]$preset2 = "Roku 480p30"
-[String]$source = "G:\Videos\MKV Videos"
+[String]$preset2 = "Roku 480p30 Modded 20rf"
+[String]$source = "F:\MKV Videos"
 [String]$destination = "G:\Videos\Jellyfin\Movies"
 [String]$sourceExt = "mkv"
 [String]$destinationExt = "mp4"
 [String]$ready = "Ready.txt"
-[String]$processed = [String]::Concat($(Split-Path -Path $source -Qualifier), "/Temp-PostProcessed")
+[String]$processed = "G:\Temp-PostProcessed"
 Start-Process -FilePath "powershell" -ArgumentList @("/c", "`$host.ui.RawUI.WindowTitle = 'handbrake.ps1' ; ./handbrake.ps1 -Encoding -Preset1 '$preset1' -Preset2 '$preset2' -Source '$source' -Destination '$destination' -SourceExt '$sourceExt' -DestinationExt '$destinationExt' -Ready '$ready' -Processed '$processed' -CheckDirectorySilent")
