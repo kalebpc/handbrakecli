2602f06c6acec48dc231329f1ea536477db16358
Kalebpc <113309697+kalebpc@users.noreply.github.com>
2026-01-07 12:07:07 -0500

    Enhance encoding script with completed files tracking
    Added functionality to track completed files globally and updated the script to check for already processed files before encoding. Remove the useless try catch in add-log function. Implement get-nextfile after every file processed. This is because this code could run for days and this fixes the issue with the code reaching for files that may or may not be there with their original names from days ago.

--- Service-EncodeMp4toWebm.ps1
+++ Service-EncodeMp4toWebm.ps1
@@ -3,6 +3,11 @@
 
     random script for encoding my movies from mp4 to webm because that seems to be the best encoding for direct playing on jellyfin.
 
+    NOTE!
+        Script uses a global var [String[]].
+
+        [String[]]$Global:completedFiles+=$x.FullName
+
 #>
 
 [CmdletBinding()]
@@ -24,7 +29,6 @@ If ( ! $(Test-Path -LiteralPath $temp) ) { New-Item -Path $temp -ItemType "Direc
 
 "Log located at:`n  '{0}'`n" -f $log
 
-
 function Add-Log {
     
     [CmdletBinding()]
@@ -37,20 +41,11 @@ function Add-Log {
         [String]$Logpath
 
     )
-    Try {
 
-        $temp = "`n------------`n[{0}] {1}`n------------`n" -f $(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt"), $Content
+    $temp = "`n------------`n[{0}] {1}`n------------`n" -f $(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt"), $Content
 
-        $temp | Out-File -LiteralPath $Logpath -Encoding unicode -Append
+    $temp | Out-File -LiteralPath $Logpath -Encoding unicode -Append
     
-    } Catch {
-
-        $Logpath -replace "(\.\b)(?!.*\1).*$",$("-{0}" -f $(Get-Date -Format "yyyy.MM.dd-hh.mm.ss-tt.log"))
-        
-        $temp | Out-File -LiteralPath $Logpath -Encoding unicode -Append
-    
-    }
-
 }
 
 function Format-Time {
@@ -93,81 +88,29 @@ If ( $Notify.Count -gt 1 ) {
 
 function Run {
 
-    Format-TimeLog "Getting new list of folders."
+    function Replace-Extension {
+    
+        [CmdletBinding()]
+        param(
 
-    ForEach ( $folder In $(Get-Childitem -LiteralPath $Folder) ) {
+            [Parameter(Mandatory = $true, Position = 0)]
+            [String]$S
 
-        function Replace-Extension {
-        
-            [CmdletBinding()]
-            param(
+        )
 
-                [Parameter(Mandatory = $true, Position = 0)]
-                [String]$S
+        return $($S -replace "(\.\b)(?!.*\1).*$",".webm")
 
-            )
+    }
 
-            return $($S -replace "(\.\b)(?!.*\1).*$",".webm")
+    function Get-NextFile {
+    
+        ForEach ( $x In $(Get-ChildItem -LiteralPath $Folder -Recurse -Depth 1 | Where { $_.Name -imatch "^.*\.mp4" } | Sort-Object) ) {
 
-        }
+            [String]$temp = Replace-Extension $x.FullName
 
-        [Object[]]$items = Get-ChildItem -LiteralPath "G:\Jellyfin\Movies" -Recurse -Depth 1 | Where { $_.Name -imatch "^.*\.mp4" } | Sort-Object
+            If ( ! $(Test-Path -LiteralPath $temp) -and $x.FullName -inotin $completedFiles ) {
 
-        If ( $items -ne $null ) {
-
-            ForEach ( $x In $items ) {
-
-                [String]$temp = Replace-Extension $x.FullName
-
-                If ( ! $(Test-Path -LiteralPath $temp) ) {
-
-                    [String]$temp = "Started: '{0}'." -f $x.FullName
-
-                    [String]$tmp = "logs\{0}.log" -f $($x.Name -replace " \[.*\] ?-?","_encode_")
-
-                    [String]$tmp = $tmp -replace "\.mp4",".webm"
-
-                    [String]$templog = $log -replace "logs.*", $tmp
-
-                    " " | Out-File -LiteralPath $templog -Encoding unicode
-
-                    Add-Log $temp $templog
-
-                    Format-TimeLog $temp
-
-                    [String]$temp = Format-TimeLog $("Started: '{0}'." -f $x.Name)
-
-                    If ($send) { $response = ./Send-Message -Content $temp -Username $result.Username -Webhookuri $result.Webhookuri ; If ( $response -inotlike "Success*" ) { $response } }
-
-                    [System.DateTime]$starttime = Get-Date
-
-                    # Encode $items to webm
-                    # HandbrakeCLI --preset-import-gui -Z "Creator 1080p30 webm" --start-at seconds:0 --stop-at seconds:5 -i $x.FullName -o $(Replace-Extension $x.FullName) 2>> $log
-                    HandbrakeCLI --preset-import-gui -Z "Creator 1080p30 webm" -i $x.FullName -o $(Replace-Extension $x.FullName) 2>> $templog
-
-                    [System.DateTime]$endtime = Get-Date
-
-                    [String]$finished = "Finished '{0}' in {1}." -f $x.Name, $(Format-Time $(New-TimeSpan -Start $starttime -End $endtime))
-
-                    [String]$temp = Format-TimeLog $finished
-
-                    $temp
-
-                    If ($send) { $response = ./Send-Message -Content $temp -Username $result.Username -Webhookuri $result.Webhookuri ; If ( $response -inotlike "Success*" ) { $response } }
-
-                    Add-Log $finished $templog
-
-                    # Short break between files to allow graceful exit.
-
-                    [String]$temp = Format-TimeLog "Sleeping for 1 minute."
-
-                    $temp
-
-                    # If ($send) { $response = ./Send-Message -Content $temp -Username $result.Username -Webhookuri $result.Webhookuri ; If ( $response -inotlike "Success*" ) { $response } }
-
-                    Start-Sleep -Seconds 60
-                
-                }
+                return $x
 
             }
 
@@ -175,8 +118,49 @@ function Run {
 
     }
 
+    [System.IO.FileInfo]$x = Get-NextFile
+
+    [String]$temp = "Started: '{0}'." -f $x.FullName
+
+    [String]$tmp = "logs\{0}.log" -f $($x.Name -replace " \[.*\] ?-? ?","_encode_")
+
+    [String]$tmp = $tmp -replace "\.mp4",".webm"
+
+    [String]$templog = $log -replace "logs.*", $tmp
+
+    If ( Test-Path -LiteralPath $templog ) { [String[]]$Global:completedFiles+=$x.FullName ; "Already completed : '{0}'." -f $templog; return }
+
+    " " | Out-File -LiteralPath $templog -Encoding unicode
+
+    Add-Log $temp $templog
+
+    Format-TimeLog $temp
+
+    [String]$temp = Format-TimeLog $("Started: '{0}'." -f $x.Name)
+
+    If ($send) { $response = ./Send-Message -Content $temp -Username $result.Username -Webhookuri $result.Webhookuri ; If ( $response -inotlike "Success*" ) { $response } }
+
+    [System.DateTime]$starttime = Get-Date
+
+    # HandbrakeCLI --preset-import-gui -Z "Creator 1080p30 webm" --start-at seconds:0 --stop-at seconds:5 -i $x.FullName -o $(Replace-Extension $x.FullName) 2>> $log
+    HandbrakeCLI --preset-import-gui -Z "Creator 1080p30 webm" -i $x.FullName -o $(Replace-Extension $x.FullName) 2>> $templog
+
+    [System.DateTime]$endtime = Get-Date
+
+    [String]$finished = "Finished '{0}' in {1}." -f $x.Name, $(Format-Time $(New-TimeSpan -Start $starttime -End $endtime))
+
+    [String]$temp = Format-TimeLog $finished
+
+    $temp
+
+    If ($send) { $response = ./Send-Message -Content $temp -Username $result.Username -Webhookuri $result.Webhookuri ; If ( $response -inotlike "Success*" ) { $response } }
+
+    Add-Log $finished $templog
+
+    [String[]]$Global:completedFiles+=$x.FullName
+
 }
 
-[Int32]$minutes = 12
+[Int32]$minutes = 1
 
 While ($true) { Run ; Format-Timelog $("Sleeping for {0} minutes." -f $minutes) ; Start-Sleep -Seconds $($minutes*60)}
