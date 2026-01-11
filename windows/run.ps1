32fe923c3093456a4e6d03fd501de4e4b55f55b0
kalebpc <113309697+kalebpc@users.noreply.github.com>
2026-01-11 18:52:39 -0500

    update for recent handbrake changes

--- windows/run.ps1
+++ windows/run.ps1
@@ -1,10 +1,8 @@
 # handbrake.ps1 © 2025 https://github.com/kalebpc/handbrakecli
 
 [Boolean]$notifying = $true
-[String]$preset1 = "Creator 1080p30 webm"
-[String]$preset2 = "Roku 480p30 Modded 20rf"
-# [String]$source = "F:\MKV Videos"
-[String]$source = "C:\Users\kaleb\Downloads"
+[String]$preset1 = "1080p30 mp4"
+[String]$source = "E:\MKV Videos"
 [String]$destination = "G:\Jellyfin\Movies"
 [String]$sourceExt = "mkv"
 [String]$destinationExt = "mp4"
@@ -12,7 +10,7 @@
 [String]$processed = "G:\Temp-PostProcessed"
 [String[]]$notify = "file",".env"
 If ($notifying) {
-    Start-Process -FilePath "powershell" -ArgumentList @("/c", "`$host.ui.RawUI.WindowTitle = 'handbrake.ps1' ; ./handbrake.ps1 -Preset1 '$preset1' -Preset2 '$preset2' -Source '$source' -Destination '$destination' -SourceExt '$sourceExt' -DestinationExt '$destinationExt' -Ready '$ready' -Processed '$processed' -Notify $($notify[0]),$($notify[1])")
+    Start-Process -FilePath "powershell" -ArgumentList @("/c", "`$host.ui.RawUI.WindowTitle = 'handbrake.ps1' ; ./handbrake.ps1 -Preset1 '$preset1' -Source '$source' -Destination '$destination' -SourceExt '$sourceExt' -DestinationExt '$destinationExt' -Ready '$ready' -Processed '$processed' -Notify $($notify[0]),$($notify[1])")
 } Else {
-    Start-Process -FilePath "powershell" -ArgumentList @("/c", "`$host.ui.RawUI.WindowTitle = 'handbrake.ps1' ; ./handbrake.ps1 -Preset1 '$preset1' -Preset2 '$preset2' -Source '$source' -Destination '$destination' -SourceExt '$sourceExt' -DestinationExt '$destinationExt' -Ready '$ready' -Processed '$processed'")
+    Start-Process -FilePath "powershell" -ArgumentList @("/c", "`$host.ui.RawUI.WindowTitle = 'handbrake.ps1' ; ./handbrake.ps1 -Preset1 '$preset1' -Source '$source' -Destination '$destination' -SourceExt '$sourceExt' -DestinationExt '$destinationExt' -Ready '$ready' -Processed '$processed'")
 }
