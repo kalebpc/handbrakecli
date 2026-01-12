1fbd56c9923464ba161afef35ce0e41aa5ffa959
kalebpc <113309697+kalebpc@users.noreply.github.com>
2026-01-11 20:23:41 -0500

    dont need to test path. just create the path and move on.

--- windows/Add-LogAndPrint.ps1
+++ windows/Add-LogAndPrint.ps1
@@ -9,24 +9,16 @@ param(
 "`n[$(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt")] {0}`n" -f $Content
 If ( $Path -ine "" ) {
     
-    If ( Test-Path -LiteralPath $Path ) {
+    Try {
 
-        Try {
+        Add-Content -LiteralPath $Path -Value "`n------------`n[$(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt")] $Content`n------------`n" -ErrorAction Stop
 
-            Add-Content -LiteralPath $Path -Value "`n------------`n[$(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt")] $Content`n------------`n"
+    } Catch {
 
-        } Catch {
-
-            $Path = "{0}\{1}-{2}" -f $($Path | Split-Path -Parent), $(Get-Random -Minimum 111111111 -Maximum 999999999 ), $($Path | Split-Path -Leaf)
-            
-            Add-Content -LiteralPath $Path -Value "`n------------`n[$(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt")] $Content`n------------`n"
+        $Path = "{0}\{1}-{2}" -f $($Path | Split-Path -Parent), $(Get-Random -Minimum 111111111 -Maximum 999999999 ), $($Path | Split-Path -Leaf)
         
-        }
+        Add-Content -LiteralPath $Path -Value "`n------------`n[$(Get-Date -Format "yyyy.MM.dd - hh:mm:ss tt")] $Content`n------------`n"
     
-    } Else {
-        
-        "`nError printing to log file."
-
     }
-    
+
 }
